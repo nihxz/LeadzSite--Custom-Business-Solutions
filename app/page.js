27dfb1b6ee532445cfa217e175f96031cb0396e8
“nihxz” <mdnihxz@gmail.com>
2025-03-29 13:52:20 +0530

    Updated home page layout and removed sections

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
 import Layout from "@/components/layout/Layout"
 import Section1 from "@/components/sections/home3/Section1"
-import Section10 from "@/components/sections/home3/Section10"
-import Section11 from "@/components/sections/home3/Section11"
-import Section12 from "@/components/sections/home3/Section12"
 import Section2 from "@/components/sections/home3/Section2"
 import Section3 from "@/components/sections/home3/Section3"
 import Section4 from "@/components/sections/home3/Section4"
@@ -11,8 +8,8 @@ import Section6 from "@/components/sections/home3/Section6"
 import Section7 from "@/components/sections/home3/Section7"
 import Section8 from "@/components/sections/home3/Section8"
 import Section9 from "@/components/sections/home3/Section9"
-export default function Home3() {
 
+export default function Home() {
     return (
         <>
             <Layout headerStyle={1} footerStyle={3} headerCls="header-style-2" topBar>
@@ -25,9 +22,6 @@ export default function Home3() {
                 <Section7 />
                 <Section8 />
                 <Section9 />
-                <Section10 />
-                <Section11 />
-                <Section12 />
             </Layout>
         </>
     )
