b51abb7e8d3f39f160574ec220953ea216513232
“nihxz” <mdnihxz@gmail.com>
2025-04-17 14:21:44 +0530

    Add Google Tag Manager integration

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,19 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                {/* Google Tag Manager */}
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-WPR9WFMT');
+                    `}
+                </Script>
+                {/* End Google Tag Manager */}
+                
+                {/* Google Analytics */}
                 <Script
                     src="https://www.googletagmanager.com/gtag/js?id=G-RK9Y22RHM0"
                     strategy="afterInteractive"
@@ -33,8 +46,21 @@ export default function RootLayout({ children }) {
                         gtag('config', 'G-RK9Y22RHM0');
                     `}
                 </Script>
+                {/* End Google Analytics */}
             </head>
-            <body className={urban.variable}>{children}</body>
+            <body className={urban.variable}>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe 
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WPR9WFMT"
+                        height="0" 
+                        width="0" 
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
+                {children}
+            </body>
         </html>
     )
 }
